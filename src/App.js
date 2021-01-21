5277384371d181f56b43795834aa3a8996627173
CanniJr <cannigialaluw@gmail.com>
2021-01-20 22:42:44 -0500

    add routes to main file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './Containers/Header'
+import Home from './Components/Home'
+import About from './Components/About'
 
 function App() {
 
   return (
     <div className="app">
-      <h1>Portfolio Time!</h1>
+      {/* Header contains navbars to other components and sticks at the top in every page */}
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path='/About'>
+            <About/>
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../CSS/About.css'
+
+function About() {
+    return (
+        <div>
+            <h1>About me Page!</h1>
+        </div>
+    )
+}
+
+export default About
